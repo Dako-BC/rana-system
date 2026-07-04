3e654240356da175a9c25a602e6098240ac8a016
Ruby Wardhani <hi.rubywardhani@gmail.com>
2026-07-04 13:21:18 +0700

    fix: root project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/rana-system/', // Match the GitHub Pages repository path.
+    base: '/',
     build: {
         outDir: 'dist',
     },
